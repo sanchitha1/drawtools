cc8c026294d84e448a62d636ee21164eafda84a9
Hashan <hemachandra.hashan@gmail.com>
2021-12-23 11:15:57 +0530

    Try Catch method

--- scripts/save_features.php
+++ scripts/save_features.php
@@ -5,18 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // checking the server request metho
     $type = $_POST['type_of_geom']; //  POST method data type
     $name = $_POST['name_of_geom']; //  POST method data type
     $string_geom = $_POST['string_of_geom']; //  POST method data type
-
-    $sql = "INSERT INTO feature_drawn (type,name,geom) VALUES ('$type','$name', ST_GeomFromGeoJSON('$string_geom'))"; // query for the record inserting
-
-    $query = $pdo->query($sql); // query executing
-
-    $successful = array("status_code" => 200); // Defining an array with key value pair
-    $unsuccessful = array("status_code" => 201); // Defining an array with key value pair
-
-    if ($query) { // if query executed sucessfully then below execute
-        echo json_encode($successful); // echo json_encode converting associative array into a JSON object from PHP variables
-    } else {
-        echo json_encode($unsuccessful); // echo json_encode converting associative array into a JSON object from PHP variables
-
+    try {
+        $sql = "INSERT INTO feature_drawn (type,name,geom) VALUES ('$type','$name', ST_GeomFromGeoJSON('$string_geom'))"; // query for the record inserting
+        $query = $pdo->query($sql); // query executing
+    } catch (PDOException $e) {
+        echo "Error <br><br>" . $e->getMessage() . "<br>";
+        echo "Line Number: " . $e->getLine();
     }
 }
