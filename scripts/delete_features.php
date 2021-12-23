adff4b8470740c9e80765db9c55d227b6954d16c
Hashan <hemachandra.hashan@gmail.com>
2021-12-23 11:04:21 +0530

    Setting the query inside a try catch method

--- scripts/delete_features.php
+++ scripts/delete_features.php
@@ -3,19 +3,14 @@ include "../includes/init.php"; // including data base connection
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // checking the server request methord, if that successful then below execute
     if (isset($_POST['id_of_feature'])) { // checking if there are data exist from the POST method 
-    $id = $_POST['id_of_feature']; //  POST method data type
-
-    $sql = "DELETE FROM feature_drawn WHERE feature_id = :id"; // query for the record delete
-    $stmnt = $pdo->prepare($sql);
-    $stmnt->execute([":id" => $id]);
-
-    $successful = array("status_code" => 300); // Defining an array with key value pair
-    $unsuccessful = array("status_code" => 301); // Defining an array with key value pair
-
-    if ($stmnt) { // if query executed sucessfully then below execute
-        echo json_encode($successful); // echo json_encode converting associative array into a JSON object from PHP variables
-    } else {
-        echo json_encode($unsuccessful); // echo json_encode converting associative array into a JSON object from PHP variables
-
+        $id = $_POST['id_of_feature']; //  POST method data type
+        try {
+            $sql = "DELETE FROM feature_drawn WHERE feature_id = :id"; // query for the record delete
+            $stmnt = $pdo->prepare($sql); // prepareing the sql as a pdo statement
+            $stmnt->execute([":id" => $id]); // executing the statement
+        } catch (PDOException $e) {
+            echo "Error <br><br>" . $e->getMessage() . "<br>";
+            echo "Line Number: " . $e->getLine();
+        }
     }
 }
