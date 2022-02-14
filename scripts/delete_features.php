64551682b2da55834afee011fa328db847a69c04
Hashan <hemachandra.hashan@gmail.com>
2022-02-14 18:09:20 +0530

    changed db table

--- scripts/delete_features.php
+++ scripts/delete_features.php
@@ -2,10 +2,10 @@
 include "../includes/init.php"; // including data base connection
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // checking the server request methord, if that successful then below execute
-    if (isset($_POST['id_of_feature'])) { // checking if there are data exist from the POST method 
+    if (isset($_POST['id_of_feature'])) { // checking if there are data exist from the POST method
         $id = $_POST['id_of_feature']; //  POST method data type
         try {
-            $sql = "DELETE FROM parcels WHERE gid = :id"; // query for the record delete
+            $sql = "DELETE FROM allparcelsnew WHERE gid = :id"; // query for the record delete
             $stmnt = $pdo->prepare($sql); // prepareing the sql as a pdo statement
             $stmnt->execute([":id" => $id]); // executing the statement
         } catch (PDOException $e) {
